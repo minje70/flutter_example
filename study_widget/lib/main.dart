d512cd1619c794ccd9d7225155062e918ce03e57
minje70 <minje70@naver.com>
2022-02-07 16:09:47 +0900

    [added] Material Components
    - 기본적으로 사용하는 Material Components에 대해서 공부해보자
    - AppBar, Scaffold, MaterialApp에 대해서 알 수 있다.

--- study_widget/lib/main.dart
+++ study_widget/lib/main.dart
@@ -1,66 +1,41 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, Key? key}) : super(key: key);
+void main() {
+  runApp(const MaterialApp(
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
+}
 
-  final Widget title;
+class TutorialHome extends StatelessWidget {
+  const TutorialHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(
-        color: Colors.blue[500],
-      ),
-      child: Row(
-        children: [
-          const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          Expanded(child: title),
-          const IconButton(
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          onPressed: null,
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+        ),
+        title: const Text('Example title'),
+        actions: const [
+          IconButton(
             onPressed: null,
             icon: Icon(Icons.search),
             tooltip: 'Search',
-          ),
+          )
         ],
+      ),
+      body: const Center(
+        child: Text('Hello, world'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
       ),
     );
   }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          const Expanded(
-              child: Center(
-            child: Text('Hello, world!'),
-          ))
-        ],
-      ),
-    );
-  }
-}
-
-void main() {
-  runApp(const MaterialApp(
-    title: 'My app',
-    home: SafeArea(
-      child: MyScaffold(),
-    ),
-  ));
 }
